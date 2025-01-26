8222cf4a85de0319cd04ed5a8c0cdd422e1273e3
ahmed.benabid <ahmed.benabid@labs.baycii.com>
2025-01-26 16:44:12 +0100

    Fix UserDao

--- src/main/java/com/example/Stocks/Repositories/UserDao.java
+++ src/main/java/com/example/Stocks/Repositories/UserDao.java
@@ -2,9 +2,11 @@ package com.example.Stocks.Repositories;
 
 import com.example.Stocks.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserDao extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
